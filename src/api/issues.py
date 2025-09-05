14766c5dea2c03def1feb2193c26ed48cd135ea0
LeoPartt <guerizec.l@gmail.com>
2025-09-05 13:28:43 +0200

    fix: remove "/api"

--- src/api/issues.py
+++ src/api/issues.py
@@ -51,7 +51,7 @@ def get_analysis_data(scan_id: str) -> Tuple[List[Any], List[Any], Optional[str]
         Tuple[warnings, rules, workspace]: Listes des warnings et rules, workspace
     """
     api_base_url = os.getenv("API_URL", "http://localhost:8001")
-    api_url = f"http://{api_base_url}/api/scans/analyse_with_rules/{scan_id}"
+    api_url = f"http://{api_base_url}/scans/analyse_with_rules/{scan_id}"
 
     logging.debug("Making GET request to API")
     logging.debug(f"   URL: {api_url}")
